317f902dfe77a9c7e1002f9bc76080ed1743bbc7
YuriNavarro <yuriacostanavarro@gmail.com>
2025-11-08 19:43:58 -0300

    Web - 08/11 - 18:43
    Esqueleto do Site

---
+++ Web/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        require_once 'header.php';
+    ?>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Crud_Mundo</h1>
+                <a href="./cidade/index.php" class="a:link">
+                    <p class="p">CRUD cidades</p>
+                </a>
+                <a href="./pais/index.php" class="a:link">
+                    <p class="p">CRUD países</p>
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
